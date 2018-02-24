a5c7bcfc3d2ffd802ead3c54a62f980699501508
joshzjc <13288723446@163.com>
2018-02-24 17:28:41 +0800

    pygal

--- python_repos.py
+++ python_repos.py
@@ -1,9 +1,27 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS,LightenStyle as LS
 
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code:",r.status_code)
 
 response_dict = r.json()
+print("Total repositories:",response_dict['total_count'])
 
-print(response_dict.keys())
+repo_dicts = response_dict['items']
+
+name,stars = [], []
+
+for repo_dict in repo_dicts:
+    name.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+
+my_style = LS('#333366',base_style=LCS)
+chart = pygal.Bar(style = my_style,x_lable_rotation = 45,show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = name
+
+chart.add('',stars)
+chart.render_to_file('python_repos.svg')
