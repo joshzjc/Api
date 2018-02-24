3e471101a9ccc6b7847dfd65e99319cb10e7198f
joshzjc <13288723446@163.com>
2018-02-24 21:02:28 +0800

    改进-one

--- python_repos.py
+++ python_repos.py
@@ -11,17 +11,29 @@ print("Total repositories:",response_dict['total_count'])
 
 repo_dicts = response_dict['items']
 
-name,stars = [], []
+names,stars = [], []
 
 for repo_dict in repo_dicts:
-    name.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
-
+    names.append(repo_dict['name'])
+    star = {
+        'value':repo_dict['stargazers_count'],
+        'label':repo_dict['description']
+    }
+    stars.append(star)
 
 my_style = LS('#333366',base_style=LCS)
-chart = pygal.Bar(style = my_style,x_lable_rotation = 45,show_legend=False)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.title_font_size = 24
+my_config.show_legend = False
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config,style = my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
-chart.x_labels = name
+chart.x_labels = names
 
 chart.add('',stars)
 chart.render_to_file('python_repos.svg')
